017a581b478354ad4dbee184833704260734791f
martinsryan <118369768+martinsryan@users.noreply.github.com>
2023-10-26 10:58:51 -0300

    Add files via upload

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,5 +1,45 @@
-document.querySelector('.tecla_pom')
-ocument.querySelector('#som_tecla_pom').play
+alert('essa pagna contem viros. Para continar faça seus dados bancários')
+function tocaSomPom(){document. querySelector('#som_tecla_').play();}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+function tocaSomPom(){document. querySelector('#som_tecla_pom').play();}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+function tocaSomClap(){document. querySelector('#som_tecla_clap').play();}
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+function tocaSomTim(){document. querySelector('#som_tecla_tim').play();}
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+
+function tocaSomPuff(){document. querySelector('#som_tecla_puff').play();}
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+
+function tocaSomSplash(){document. querySelector('#som_tecla_splash').play();}
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+function tocaSomToim(){document. querySelector('#som_tecla_toim').play();}
+document.querySelector('.tecla_tiom').onclick = tocaSomToim;
+
+function tocaSomPsh(){document. querySelector('#som_tecla_psh').play();}
+document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+
+function tocaSomTic(){document. querySelector('#som_tecla_tic').play();}
+document.querySelector('.tecla_tic').onclick = tocaSomTic;
+
+function tocaSomTom(){document. querySelector('#som_tecla_tom').play();}
+document.querySelector('.tecla_tom').onclick = tocaSomTom;
+
+
+
+const Listadeteclas = document.querySelectorAll('.tecla');
+
+
+
+
+
+ 
+
+
+document.querySelector('#som_tecla_pom').play
 document.querySelector('.tecla_clap')
 document.querySelector('.tecla_tim')
 document.querySelector('.tecla_puff')
@@ -8,3 +48,7 @@ document.querySelector('.tecla_toim')
 document.querySelector('.tecla_psh')
 document.querySelector('.tecla_tic')
 document.querySelector('.tecla_tom')
+
+
+
+    
